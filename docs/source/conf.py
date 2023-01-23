c29d1611635a6470dd16f4dabd918a2a6cd27c14
aniket singh rawat <aniket.rawat@bytelearn.ai>
2023-01-23 21:45:52 +0530

    updated logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
+html_static_path = ["_static"]
+html_logo = "pylops.png"
+
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
